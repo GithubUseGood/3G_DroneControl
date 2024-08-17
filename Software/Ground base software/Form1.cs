7caefb9dc6ce7cc3c9ff63d78ab2cf23022fb5db
GithubUseGood <117121248+GithubUseGood@users.noreply.github.com>
2024-08-17 12:27:46 +0200

    initial commit

---
+++ Software/Ground base software/Form1.cs
@@ -0,0 +1,10 @@
+namespace Ground_base_software
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Software/ZOHD airplane software/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ZOHD_airplane_software
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ZOHD airplane side ");
+        }
+    }
+}
